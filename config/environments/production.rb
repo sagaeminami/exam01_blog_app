6f72554225607109bf9a8dd25aa88da40a5b9054
寒河江陽 <sagaeminami@sagaeakiranoMacBook-Pro.local>
2017-11-07 15:23:36 +0900

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'limitless-lake-24619.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'limitless-lake-24619' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
