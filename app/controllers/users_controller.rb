92aea855ec961e9bdc2f2f7bc23f20dbdc7a3d60
寒河江陽 <sagaeminami@sagaeakiranoMacBook-Pro.local>
2017-11-10 16:36:28 +0900

    image uopload

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   def update
    @user = User.find(params[:id])
    if @user.update(user_params)
-     redirect_to user_show_path(@user)
+     redirect_to user_path
    else
     render :edit
    end
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
      if @user.save
-       redirect_to user_path(@user.id),notice:'ログインしました！'
+       redirect_to user_path(@user.id),notice:'登録しました！'
      else
        render 'new'
      end
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
 
   private
    def user_params
-     params.require(:user).permit(:name,:email,:password,:password_confirmation,:image)
+     params.require(:user).permit(:name,:email,:password,:password_confirmation,:image,:image_cache)
    end
 
 end
